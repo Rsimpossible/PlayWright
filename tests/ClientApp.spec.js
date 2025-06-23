160783299841a34007e56412cb1c79ad206c44e4
Rsimpossible <rahulsrivastav76@gmail.com>
2025-06-23 12:22:11 +0530

    V2

--- tests/ClientApp.spec.js
+++ tests/ClientApp.spec.js
@@ -3,12 +3,28 @@ const {test, expect} = require('@playwright/test');
 
 test('Browser context Playwright test', async ({page})=>
 {
+        const productName = 'Fav Gucci'
+        const products = page.locator(".card-body");
         await page.goto("https://rahulshettyacademy.com/client");
         await page.locator("#userEmail").fill("anshika@gmail.com");
         await page.locator("#userPassword").fill("Iamking@000");
         await page.locator("[value='Login']").click();
-        //await page.waitForLoadState('networkidle'); - discouraged step 
+        await page.waitForLoadState('networkidle');
         //await page.locator(".card-body b").first().waitFor();
         const titles= await page.locator(".card-body b").allTextContents();
         console.log(titles);
+        const count = await products.count();
+        for(let i = 0; i < count; ++i)
+        {
+        if (await products.nth(i).locator("b").textContent()===productName)
+                {
+                await products.nth(i).locator("text= Add To Cart").click();
+                break;
+                }
+        }
+        await page.locator("[routerlink*='cart']").click();
+        await page.locator("div li").first().waitFor();
+        const bool =await page.locator("h3:has-text('Fav Gucci')").isVisible();
+        expect(bool).toBeTruthy();
+        await page.locator("text=Checkout").click();
 });
