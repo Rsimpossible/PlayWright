596dfaf660461112495e1debed853e21c44b4b5f
Rsimpossible <rahulsrivastav76@gmail.com>
2025-06-30 12:24:23 +0530

    V11

--- tests/morevalidations.spec.js
+++ tests/morevalidations.spec.js
@@ -10,4 +10,7 @@ test("popup validations",async({page})=>
     await expect(page.locator("#displayed-text")).toBeVisible();
     await page.locator("#hide-textbox").click();
     await expect(page.locator("#displayed-text")).toBeHidden();
+    page.on("dialog",dialog => dialog.accept());
+    await page.locator("#confirmbtn").click();
+    await page.locator("#mousehover").click();
 });
