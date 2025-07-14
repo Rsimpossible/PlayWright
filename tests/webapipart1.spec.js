28e795becc227b2f7d777ba19f7b42cbd80675ce
Rsimpossible <rahulsrivastav76@gmail.com>
2025-07-14 10:45:41 +0530

    V14

--- tests/webapipart1.spec.js
+++ tests/webapipart1.spec.js
@@ -1,81 +1,46 @@
 const {test, expect, request} = require('@playwright/test');
-const loginPayLoad = {userEmail: "rowl86@hotmail.com", userPassword: "Rahul@sri86"};
-
-test.beforeAll( async ()=>
+const {APiUtils} = require('../utils/APiUtils');
+const loginPayLoad = {userEmail:"anshika@gmail.com",userPassword:"Iamking@000"};
+const orderPayLoad = {orders:[{country:"Cuba",productOrderedId:"67a8dde5c0d3e6622a297cc8"}]};
+ 
+ 
+let response;
+test.beforeAll( async()=>
 {
-    const apiContext = await request.newContext();
-    const loginResponse = await apiContext.post("https://rahulshettyacademy.com/api/ecom/auth/login", 
-        {
-            data:loginPayLoad
-        } )
-        expect(loginResponse.ok())
-    const loginResponseJson = await loginResponse.json();
-    const token = loginResponseJson.token;
-    console.log(token);
-
-});
-
-test.beforeEach( () =>
+   const apiContext = await request.newContext();
+   const apiUtils = new APiUtils(apiContext,loginPayLoad);
+   response =  await apiUtils.createOrder(orderPayLoad);
+ 
+})
+ 
+ 
+//create order is success
+test('@API Place the order', async ({page})=>
+{ 
+    page.addInitScript(value => {
+ 
+        window.localStorage.setItem('token',value);
+    }, response.token );
+await page.goto("https://rahulshettyacademy.com/client");
+ await page.locator("button[routerlink*='myorders']").click();
+ await page.locator("tbody").waitFor();
+const rows = await page.locator("tbody tr");
+ 
+ 
+for(let i =0; i<await rows.count(); ++i)
 {
-
+   const rowOrderId =await rows.nth(i).locator("th").textContent();
+   if (response.orderId.includes(rowOrderId))
+   {
+       await rows.nth(i).locator("button").first().click();
+       break;
+   }
+}
+const orderIdDetails =await page.locator(".col-text").textContent();
+//await page.pause();
+expect(response.orderId.includes(orderIdDetails)).toBeTruthy();
+ 
 });
-
-
-test('Browser context Playwright test', async ({page})=>
-{
-        page.addInitScript(value => {
-            window.localStorage.setItem('token', value);
-        }, token );
-        //await page.locator(".card-body b").first().waitFor();
-        const titles= await page.locator(".card-body b").allTextContents();
-        console.log(titles);
-        const count = await products.count();
-        for(let i = 0; i < count; ++i)
-        {
-        if (await products.nth(i).locator("b").textContent()===productName)
-                {
-                await products.nth(i).locator("text= Add To Cart").click();
-                break;
-                }
-        }
-        await page.locator("[routerlink*='cart']").click();
-        await page.locator("div li").first().waitFor();
-        const bool =await page.locator("h3:has-text('Fav Gucci')").isVisible();
-        expect(bool).toBeTruthy();
-        await page.locator("text=Checkout").click();
-        await page.locator("[placeholder*='Country']").pressSequentially("ind",{delay:100});
-        const dropdown = page.locator(".ta-results");
-        await dropdown.waitFor();
-        await dropdown.locator("button").count();
-        const optionsCount = await dropdown.locator("button").count();
-        for (let i = 0; i < optionsCount; ++i)
-        {
-                const text = await dropdown.locator("button").nth(i).textContent();
-                if (text == " India")
-                        {
-                                await dropdown.locator("button").nth(i).click();
-                                break;
-                        }
-        }
-                expect(page.locator(".user__name [type='text']").first()).toHaveText(email);
-                await page.locator(".action__submit").click();
-                await expect(page.locator(".hero-primary")).toHaveText(" Thankyou for the order. ");
-                const orderID = await page.locator(".em-spacer-1 .ng-star-inserted").textContent();
-                console.log(orderID);
-                await page.locator("button[routerlink*='myorders']").click();
-                await page.locator("tbody").waitFor();
-                const rows = await page.locator("tbody tr");
-                for(let i = 0; i<await rows.count(); ++i)
-                {
-                        const rowOrderID = await rows.nth(i).locator("th").textContent();
-                        if (orderID.includes(rowOrderID))
-                        {
-                                await rows.nth(i).locator("button").first().click();
-                                break;
-                        }
-                }
-                const orderIDDetails = await page.locator(".col-text").textContent();
-                expect(orderID.includes(orderIDDetails)).toBeTruthy
-                
-        
-});
+ 
+//Verify if order created is showing in history page
+// Precondition - create order -
